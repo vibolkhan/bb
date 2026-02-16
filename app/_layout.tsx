37fb544281ddba06be4675636174c83534d5fd70
Vibol Khan <vibol.khan.mail@gmail.com>
2026-02-16 13:55:40 +0700

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,10 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 
 export const unstable_settings = {
   anchor: '(tabs)',
